fd5a3951afe7f1bf7cf45f87f539b286679147c1
Adrian Tepsan <adriantepsan@gmail.com>
2023-04-03 12:56:41 +0300

    Add main page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'outlet';
+
+  constructor() {}
+
+  goToLogin(): void {
+    console.log("go to login ...");
+  }
 }
