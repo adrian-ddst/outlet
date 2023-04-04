aa76921a184ce929d6861429c85869c1a7f9dc5c
Adrian Tepsan <adriantepsan@gmail.com>
2023-04-04 14:58:41 +0300

    Add items list page

--- src/app/pages/choose-one-specific/choose-one-specific.component.ts
+++ src/app/pages/choose-one-specific/choose-one-specific.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-choose-one-specific',
@@ -6,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./choose-one-specific.component.scss']
 })
 export class ChooseOneSpecificComponent implements OnInit {
-  constructor() {}
+  category: string | null | undefined;
+  specificCategory: string | null | undefined;
+  chooseOneSpecificRouter: Router;
+
+  filterList = ["Filter Type 1", "Filter Type 2", "Filter Type 3", "Filter Type 4", "Filter Type 5"];
+  items = ["Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket", "Armani Exchange Men's Jacket"];
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
+    this.chooseOneSpecificRouter = router;
+  }
 
   ngOnInit(): void {
-    console.log("im up!")
+    this.route.params.subscribe(res => {
+      this.category = ((res['category'] ? res['category'] : "unknown") as string).toUpperCase();
+      this.specificCategory = ((res['specificCategory'] ? res['specificCategory'] : "unknown") as string).toUpperCase();
+    });
   }
+
 }
