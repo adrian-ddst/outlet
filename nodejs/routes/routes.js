4ff43627b53cd756da3b05b4f35ee6f3f8748151
Adrian Tepsan <adriantepsan@gmail.com>
2023-05-05 12:37:28 +0300

    Add documentation for uploading images in cloudinary in /debugPost endpoint function

--- nodejs/routes/routes.js
+++ nodejs/routes/routes.js
@@ -195,7 +195,25 @@ router.get('/debugGet', async (req, res) => {
 router.post('/debugPost', async (req, res) => {
     res.set(allowCORS, frontendURL);
     console.log("Received request to ['/debugPost'] ... ");
+
+    // test on how to upload images to cloudinary ...
+    // *[ imageUrl ] este url-ul imaginii (poate fi blob)
+    // *[ {} ] e parametrul "options" al functiei de upload
+    // *[ public_id ] este numele imaginii in cloudinary (va trebui sa fie unic)
+    // *[ folder ] este numele folderului in care il pune, o sa pastram "outlet-clothes-images"
+    // *[ secureUrl ] va fi url-ul la care se gaseste imaginea... va trebui pus in db pe product
     try {
+        const imageUrl = "https://upload.wikimedia.org/wikipedia/commons/a/ae/Olympic_flag.jpg";
+        const upload = cloudinary.uploader.upload(imageUrl, { public_id: "test_flag", folder: "outlet-clothes-images" });
+        var secureUrl = '';
+        upload.then((data) => {
+            if (data?.secure_url) {
+                secureUrl = data.secure_url;
+                console.log("Image upload was successful ... URL: " + secureUrl);
+            }
+        }).catch((err) => {
+            console.log("Image upload has failed ... error: ", err);
+        });;
         res.status(200).json({ message: "ok" });
     } catch (err) {
         res.status(500).json(err);
